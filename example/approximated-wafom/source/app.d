8551559c8cfab9ff23666a885a0d8230019d87eb
majiang <neuron.ohori@gmail.com>
2015-03-04 17:39:26 +0900

    Add example of approximated WAFOM

---
+++ example/approximated-wafom/source/app.d
@@ -0,0 +1,22 @@
+import std.stdio, std.conv, std.algorithm, std.array, std.string;
+
+import wafom.approximated;
+import digitalnet.implementation;
+
+void main(string[] args)
+{
+	real[] c;
+	if (args.length == 1)
+	{
+		stderr.writeln("approximated-wafom c...");
+		return;
+	}
+	c = args[1..$].map!(to!real).array;
+	foreach (line; stdin.byLine)
+	{
+		line.strip.split(",")[0].write;
+		foreach (cvalue; c)
+			",%.15f".writef(line.toDigitalNet.WAFOM(cvalue).lg);
+		writeln;
+	}
+}
